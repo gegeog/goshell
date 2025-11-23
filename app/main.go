1de0f48fb8726428ad4601ff5c19c5b2132b25b0
gegeog <KartashovVD@yandex.ru>
2025-11-23 21:24:10 +0700

    [any message]

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -11,4 +12,10 @@ var _ = os.Stdout
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
